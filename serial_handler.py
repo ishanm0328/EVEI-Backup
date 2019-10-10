4aa8b5a076d7acfc8802c181c46fe3970fcf0086
Caleb Smith <karagenit@outlook.com>
2019-10-10 11:25:35 -0400

    Fix Serial Data Parser
    Separate Lap Count & Energy

--- serial_handler.py
+++ serial_handler.py
@@ -13,7 +13,8 @@ def parse(serial_string):
                '[\s]*'              # Match any number of spaces after that
                'A([0-9]\.[0-9])'    # Match A0.0, A1.3, etc
                'B([0-9]\.[0-9])'    # Match B0.0, B1.3, etc
-               'D\nE([0-9]+)'       # Match DE0, DE100, etc - for some reason, the D is on line 1 and the E is on line 2
+               '([D ])\n'           # Match D or a blank
+               'E([0-9]+)'          # Match E0, E100, etc
                '[\s]*'              # Match any number of spaces after
                '([+-][0-9]{1,2})'   # Match +3, -12, etc
                '[\s]?'              # There might be a space, if the number before was only 1 digit
@@ -30,10 +31,11 @@ def parse(serial_string):
     data['rpm'] = int(matches.group(1))
     data['analog_a'] = float(matches.group(2))
     data['analog_b'] = float(matches.group(3))
-    data['lap_count'] = int(matches.group(4))
-    data['gyro_x'] = int(matches.group(5)) # TODO: x/y might be wrong
-    data['gyro_y'] = int(matches.group(6))
-    data['gyro_z'] = int(matches.group(7))
+    data['lap_count'] = bool(matches.group(4).strip())
+    data['energy'] = int(matches.group(5))
+    data['gyro_x'] = int(matches.group(6)) # TODO: x/y might be wrong
+    data['gyro_y'] = int(matches.group(7))
+    data['gyro_z'] = int(matches.group(8))
     return data
 
 ###
