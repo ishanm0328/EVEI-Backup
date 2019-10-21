5be2277d948a8281504eeffa28ac2cede3db44dd
Caleb Smith <karagenit@outlook.com>
2019-10-21 12:48:25 -0400

    Add Main Program & Sample Window Class

---
+++ main.py
@@ -0,0 +1,24 @@
+import config
+from serial_handler import SerialHandler
+from window import Window
+from graph import Graph
+import tkinter as tk
+
+# TODO: setup root with toolbars/buttons
+
+# TODO: setup window with graphs
+
+config = config.get_config
+data = []
+serial = SerialHandler(data, config)
+window = Window() # Tk window object
+graph = Graph(data, window)
+
+while True:
+    #serial.update()
+   window.update_idletasks()
+   window.update()
+
+# TODO: register exit handler
+save_config(config)
+# TODO: save
---
+++ window.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
