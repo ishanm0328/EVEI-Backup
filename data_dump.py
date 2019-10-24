b3b09121b4a9ce22253043a566426c2979bed3cc
SOGIST1 <47545935+SOGIST1@users.noreply.github.com>
2019-10-24 11:37:42 -0400

    Update data_dump.py
    Iterates through the keys to create the first line which titles the csv file

--- data_dump.py
+++ data_dump.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python3
-import serial
-import serial_handler as sh
+def data_dump(data): #this function is expecting a list of dictionaries
+    sampleDataKeys = list(data[0].keys())#makes a list of the keys of the first dictionary of data in the data array
+    firstLine = ""#initializes the first line variable which will hold all the information to be written in the first line
+    for i in range(len(sampleDataKeys)):#iterates through each key in the list
+        firstLine = firstLine + sampleDataKeys[i]+','#adds each key separated by a comma into the firstLine variable
+    
+    with open('data.csv', 'w') as o:#opens a file named data.csv as writtable
+       o.write(firstLine+"\n")#writes the first line into the csv file
+       for i in range(len(data)):#iterates through each datapoint in the given array of dictionaries from the function parameter
+            o.write(','.join(map(str,list(data[i].values())))+'\n')#writes the values of each dictionary in the array to the csv file
+            
+'''
+#This code is for testing the function:
+            
+sampledict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+        }
 
-def data_dump(data):
-    with open('data.csv', 'w') as o:
-        for i in range(len(data)):
-            o.write(','.join(map(str,list(data[i].values())))+'\n')
+sampledict2 = {
+        "brand": "Chevy",
+        "model": "Camero",
+        "year": 1988
+        }
+
+sampledata = [sampledict, sampledict2]
+
+
+data_dump(sampledata)
+'''
