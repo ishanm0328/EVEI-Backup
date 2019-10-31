aa3640a99f686b54fb84b783d6330fb158beefdd
ztwilliams197 <46409079+ztwilliams197@users.noreply.github.com>
2019-10-31 10:40:42 -0400

    Add files via upload

--- graph.py
+++ graph.py
@@ -10,14 +10,35 @@ class Graph():
     def __init__(self, window, data):
         self.window = window
         self.data = data
+        self.graphNames = [["RPM","Energy"],["Acceleration","Position"]]
 
-        self.figure = Figure(figsize=(5,4), dpi=100)
-        t = np.arange(0, 3, .01)
-        self.figure.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+        self.figure, self.axs = plt.subplots(2,2)
+        setGraphNames(self.axs, graphNames)
 
         self.canvas = FigureCanvasTkAgg(self.figure, master=window)  # A tk.DrawingArea.
         #self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
     def update():
-        pass
+        for i in range(len(graphNames)):
+            for k in range(len(graphNames[0])):
+                yar = []
+                if(len(data) <= 10):
+                    xar = np.arange(1,11)
+                    for j in range(10):
+                        yar.append(data[j][graphNames[i][k]])
+                else:
+                    xar = np.arange(len(data)-10,len(data)+1)
+                    for j in range(-10:0):
+                        yar.append(data[j][graphNames[i][k]])
+                axs[i,k].clear()
+                setGraphNames(axs,graphNames)
+                axs[i,k].plot(xar,yar)
+        self.canvas.draw()
+
+    def setGraphNames(axs,graphNames):
+        for i in range(len(graphNames)):
+            for k in range(len(graphNames[0])):
+                axs[i,k].set_title(graphNames[i][k])
+                axs[i,k].set_ylabel(graphNames[i][k])
+                axs[i,k].set_xlabel("Time")
