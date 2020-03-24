14546533ab2ae318c29b49fac9d2a4b858206127
ztwilliams197 <46409079+ztwilliams197@users.noreply.github.com>
2020-03-23 22:03:14 -0400

    Update window.py

--- window.py
+++ window.py
@@ -7,8 +7,19 @@ class Window(tk.Tk):
         super(Window, self).__init__(*args, **kwargs)
 
     def prompt_serial(self):
-        filetypes = [('serial ports', 'ttyS*'), ('usb serial', 'ttyUSB*')]
-        port = filedialog.askopenfilename(parent=self, initialdir='/dev',
-                                   title='Select Serial Port',
-                                   filetypes=filetypes)
-        self.config['serial']['port'] = port
+        if platform == "linux" or platform == "linux2":    
+            filetypes = [('serial ports', 'ttyS*'), ('usb serial', 'ttyUSB*')]
+            port = filedialog.askopenfilename(parent=self, initialdir='/dev',
+                                       title='Select Serial Port',
+                                       filetypes=filetypes)
+            self.config['serial']['port'] = port
+        elif platform == "win32":
+            pattern = re.compile(r'COM\d')
+        
+            query = "SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%(COM%)'"
+            coms  = wmi.WMI().query(query)
+        
+            name = []
+            for com in coms:
+                name.append(re.search(pattern, com.Name, flags = 0).group())
+                print(name)
